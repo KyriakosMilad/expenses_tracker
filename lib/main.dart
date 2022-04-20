f07423adb00c59c93d872215ad49da99c6e47ead
Kyriakos Milad <contact.kyriakos@gmail.com>
2022-04-20 18:55:50 +0200

    Update app name and AppBar title

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Personal Expenses',
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      home: MyHomePage('Flutter Demo Home Page'),
+      home: MyHomePage('Personal Expenses'),
     );
   }
 }
